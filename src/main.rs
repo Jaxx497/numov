45a50d8e4cabab4461e0fa506d25cfca5188bc7c
Jaxx497 <equus497@gmail.com>
2024-03-17 01:52:56 -0600

    Began implementation of cmdline parsing

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,74 @@ mod library;
 mod movie;
 mod movie_types;
 
+use clap::Parser;
+use clap::ValueEnum;
 use library::Library;
-use std::time::Instant;
+use std::{path::PathBuf, time::Instant};
 
 fn main() {
-    let t1 = Instant::now();
+    let args = Args::parse();
+    println!("{:?}", args);
 
-    let mut lib = Library::new("M:/");
+    // let lib = if let Some(root) = args.root {
+    //     if PathBuf::from(&root).is_dir() {
+    //         Library::new(root)
+    //     } else {
+    //         eprintln!("Error: Invalid root directory specified.");
+    //         std::process::exit(1);
+    //     }
+    // } else {
+    //     eprintln!("Error: Invalid root directory specified.");
+    //     std::process::exit(1);
+    // };
 
+    // let t1 = Instant::now();
+    //
+    // let mut lib = Library::new("M:/");
+    //
     // lib.update_ratings("equus497").unwrap_or_default();
-    lib.update_movies();
+    // lib.update_movies();
+    //
+    // lib.rename_folders();
+    //
+    // println!("\nCompleted all tasks in {:.4?}", Instant::now() - t1);
+}
 
-    lib.rename_folders();
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+struct Args {
+    /// Root directory of movie library
+    #[arg(short, long)]
+    root: Option<String>,
 
-    println!("\nCompleted all tasks in {:.4?}", Instant::now() - t1);
+    /// Letterboxd User we will try to scrape
+    #[arg(short, long = "letterboxd")]
+    lb_user: Option<String>,
+
+    /// Output data in a given format
+    #[arg(short, long, value_enum)]
+    output: Option<Output>,
+
+    #[arg(long, action = clap::ArgAction::SetTrue)]
+    csv: bool,
+    #[arg(long,  action = clap::ArgAction::SetTrue)]
+    json: bool,
+    #[arg(long = "df", action = clap::ArgAction::SetTrue)]
+    df: bool,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum Output {
+    Csv,
+    Json,
+    Df,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum Update {
+    M,
+    Movies,
+    R,
+    Ratings,
+    All,
 }
